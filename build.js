d50a72865911a8240053cf6f1d3484441ab9e291
goatcats <78546026+goatcats@users.noreply.github.com>
2023-06-23 18:34:31 +0200

    Update build.js

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ const path = process.argv[1].replace("build.js","");
 const content = `#!/bin/sh\nnode ${path}main.mjs $@`;
 const dest = "/usr/bin/todo";
 
+console.log("> building...");
 fs.writeFileSync(dest, content, {flag:'w'});
 fs.chmodSync(dest, "755");
 
