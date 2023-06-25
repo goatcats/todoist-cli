429c478f2cab697b4052fd9c47a3592428f6bbee
goatcats <78546026+goatcats@users.noreply.github.com>
2023-06-25 19:26:50 +0200

    added time feature

--- main.mjs
+++ main.mjs
@@ -5,7 +5,7 @@ const input = promptSync();
 import CG from 'console-grid';
 import chalk from 'chalk';
 
-let projects, project // 5d0fcc3adbe9ed4a397f8d88a26089c3102f608d
+let projects, project
 
 const path = process.argv[1].replace("main.mjs", "");
 
@@ -57,6 +57,17 @@ const path = process.argv[1].replace("main.mjs", "");
     const args = process.argv.slice(2);
     const api = new TodoistApi(data.api_key);
 
+    function incTime(timeString, hours) {
+        const [hoursStr, minutesStr] = timeString.split(":");
+        let totalMinutes = parseInt(hoursStr) * 60 + parseInt(minutesStr);
+        totalMinutes += hours * 60;
+        const newHours = Math.floor(totalMinutes / 60) % 24;
+        const newMinutes = totalMinutes % 60;
+        const paddedHours = String(newHours).padStart(2, "0");
+        const paddedMinutes = String(newMinutes).padStart(2, "0");
+        return `${paddedHours}:${paddedMinutes}`;
+    }
+
     async function list_tasks(callback) {
         if (process.stdout.columns < 95) {
             console.log("Terminal is too narrow.\nRequired width: 95");
@@ -79,20 +90,20 @@ const path = process.argv[1].replace("main.mjs", "");
             if (!tasks[i].due) {
                 rows.push([chalk.gray(i), chalk.gray("--"), chalk.gray(tasks[i].priority), chalk.gray(correct(tasks[i].content.length>29 ? tasks[i].content.slice(0, 27)+"..." : tasks[i].content)), chalk.gray(tasks[i].description === "" || tasks[i].description === null ? "--" : correct(tasks[i].description.length>29 ? tasks[i].description.slice(0, 27)+"..." : tasks[i].description))]);
             } else if (isToday(tasks[i])) {
-                rows.push([chalk.green(i), chalk.green(new Date(tasks[i].due.date).toDateString()), chalk.green(tasks[i].priority), chalk.green(correct(tasks[i].content.length>29 ? tasks[i].content.slice(0, 27)+"..." : tasks[i].content)), chalk.green(tasks[i].description === "" || tasks[i].description === null ? "--" : correct(tasks[i].description.length>29 ? tasks[i].description.slice(0, 27)+"..." : tasks[i].description))]);
+                rows.push([chalk.green(i), chalk.green(new Date(tasks[i].due.date).toDateString()), chalk.green(!!tasks[i].due.datetime?incTime(tasks[i].due.datetime.slice(11,16),2):"--"), chalk.green(tasks[i].priority), chalk.green(correct(tasks[i].content.length>29 ? tasks[i].content.slice(0, 27)+"..." : tasks[i].content)), chalk.green(tasks[i].description === "" || tasks[i].description === null ? "--" : correct(tasks[i].description.length>29 ? tasks[i].description.slice(0, 27)+"..." : tasks[i].description))]);
             } else if (isOverdue(tasks[i])) {
-                rows.push([chalk.red(i), chalk.red(new Date(tasks[i].due.date).toDateString()), chalk.red(tasks[i].priority), chalk.red(correct(tasks[i].content.length>29 ? tasks[i].content.slice(0, 27)+"..." : tasks[i].content)), chalk.red(tasks[i].description === "" || tasks[i].description === null ? "--" : correct(tasks[i].description.length>29 ? tasks[i].description.slice(0, 27)+"..." : tasks[i].description))]);
+                rows.push([chalk.red(i), chalk.red(new Date(tasks[i].due.date).toDateString()), chalk.red(!!tasks[i].due.datetime?incTime(tasks[i].due.datetime.slice(11,16),2):"--"), chalk.red(tasks[i].priority), chalk.red(correct(tasks[i].content.length>29 ? tasks[i].content.slice(0, 27)+"..." : tasks[i].content)), chalk.red(tasks[i].description === "" || tasks[i].description === null ? "--" : correct(tasks[i].description.length>29 ? tasks[i].description.slice(0, 27)+"..." : tasks[i].description))]);
             } else {
-                rows.push([chalk.blue(i), chalk.blue(new Date(tasks[i].due.date).toDateString()), chalk.blue(tasks[i].priority), chalk.blue(correct(tasks[i].content.length>29 ? tasks[i].content.slice(0, 27)+"..." : tasks[i].content)), chalk.blue(tasks[i].description === "" || tasks[i].description === null ? "--" : correct(tasks[i].description.length>29 ? tasks[i].description.slice(0, 27)+"..." : tasks[i].description))]);
+                rows.push([chalk.blue(i), chalk.blue(new Date(tasks[i].due.date).toDateString()), chalk.blue(!!tasks[i].due.datetime?incTime(tasks[i].due.datetime.slice(11,16),2):"--"), chalk.blue(tasks[i].priority), chalk.blue(correct(tasks[i].content.length>29 ? tasks[i].content.slice(0, 27)+"..." : tasks[i].content)), chalk.blue(tasks[i].description === "" || tasks[i].description === null ? "--" : correct(tasks[i].description.length>29 ? tasks[i].description.slice(0, 27)+"..." : tasks[i].description))]);
             }
         }
 
         CG({
-            "columns": [chalk.bold(tasks.length), chalk.bold("Date"), chalk.bold("P"), chalk.bold("Task"), chalk.bold("Description")],
+            "columns": [chalk.bold(tasks.length), chalk.bold("Date"), chalk.bold("Time"), chalk.bold("P"), chalk.bold("Task"), chalk.bold("Description")],
             "rows": rows
         });
 
-        await callback(tasks, data);
+        await callback(tasks, data, incTime);
     }
 
     switch (args[0]) {
@@ -102,7 +113,7 @@ const path = process.argv[1].replace("main.mjs", "");
 
         case "e":
             let help = false;
-            await list_tasks(async (tasks, confData) => {
+            await list_tasks(async (tasks, confData, incTime) => {
                 console.log(`Syntax: <number> <option> [value] | help`);
                 const inpString = input(">> ");
                 if (!inpString) {process.exit(1)}
@@ -117,7 +128,7 @@ const path = process.argv[1].replace("main.mjs", "");
 - done (complete a task)`);
                         return "help";
                     } else if (0 <= inpArgs[0] && inpArgs[0] < tasks.length) {
-                        if ("name description date priority done".includes(inpArgs[1])) {
+                        if ("name description date time priority done".includes(inpArgs[1])) {
                             switch (inpArgs[1]) {
                                 case "name": inpArgs[1] = "content"; break;
                                 case "date": inpArgs[1] = "dueDate"; break;
@@ -130,6 +141,17 @@ const path = process.argv[1].replace("main.mjs", "");
                     const toChange = data.option;
                     if (toChange === "done") {
                         await api.closeTask(data.task_id); return;
+                    } else if (toChange === "time") {
+                        if (data.val[0]==="" || data.val===[]) {
+                            console.log((await api.getTask(data.task_id)).due.date);
+                            await api.updateTask(data.task_id, {
+                                dueDate: (await api.getTask(data.task_id)).due.date
+                            }); return;
+                        } else {
+                            await api.updateTask(data.task_id, {
+                                dueDatetime:`${(await api.getTask(data.task_id)).due.date}T${incTime(data.val[0],-2)}:00Z`
+                            }); return;
+                        }
                     }
                     const updateObj = {};
                     let value = data.val.join(" ");
@@ -151,14 +173,15 @@ const path = process.argv[1].replace("main.mjs", "");
 
         case "a":
             
-            await list_tasks(async (tasks, confData)=>{
+            await list_tasks(async (tasks, confData, incTime)=>{
                 const vName = (name) => name && name.length < 30 && name.length > 2;
                 const vDescription = (description) => description.length < 45;
-                const vDate = (date) => date && /^\d{4}-\d{2}-\d{2}$/.test(date);
-                const vPriority = (priority) => priority && priority >= 1 && priority <= 4;
+                const vDate = (date) => (date && /^\d{4}-\d{2}-\d{2}$/.test(date));
+                const vTime = (time) => (time && /^([01]\d|2[0-3]):([0-5]\d)$/.test(time) || time==="");
+                const vPriority = (priority) => (priority && priority >= 1 && priority <= 4);
                 
                 const data = (() => {
-                    let name, description, date, priority;
+                    let name, description, date, time, priority;
                     while (true) {
                         name = input("Name: ");
                         if (!name) {process.exit(1)}
@@ -166,20 +189,33 @@ const path = process.argv[1].replace("main.mjs", "");
                         if (!description) {description = ""}
                         date = input("Date (YYYY-MM-DD): ");
                         if (!date) {process.exit(1)}
+                        time = input("Time: ");
+                        if (!time) {time=""}
                         priority = input("Priority: ");
-                        if (!priority) {process.exit(1)}
-                        if (vName(name) && vDescription(description) && vDate(date) && vPriority(priority)) {break}
+                        if (!priority) {priority = 1}
+                        if (vName(name) && vDescription(description) && vDate(date) && vTime(time)  && vPriority(priority)) {break}
                         else {console.log(chalk.red("Invalid values\n"))}
                     }
-                    return {name, description, date, priority};
-                })();
-                await api.addTask({
-                    content: data.name,
-                    description: data.description,
-                    dueDate: data.date,
-                    priority: data.priority,
-                    projectId: confData.selected.id
-                });
+                    return {name, description, date, time, priority};
+                })()
+
+                if (data.time === "") {
+                    await api.addTask({
+                        content: data.name,
+                        description: data.description,
+                        dueDate:data.date,
+                        priority: data.priority,
+                        projectId: confData.selected.id
+                    });
+                } else {
+                    await api.addTask({
+                        content: data.name,
+                        description: data.description,
+                        dueDatetime:`${data.date}T${incTime(data.time,-2)}:00Z`,
+                        priority: data.priority,
+                        projectId: confData.selected.id
+                    });
+                }
             });
             await list_tasks(()=>{});
             break;
